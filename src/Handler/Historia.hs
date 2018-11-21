7d09921ec5a4d1ee85f20ed951bd7fac69cadbd7
Haryel Assencao <haryel.assencao@fatec.sp.gov.br>
2018-11-21 22:28:20 +0000

    paginas de historia

--- src/Handler/Historia.hs
+++ src/Handler/Historia.hs
@@ -10,9 +10,40 @@ import Text.Lucius
 import Text.Julius
 import Import
 
+formHistoria :: Form Historia
+formHistoria = renderBootstrap $ Historia 
+    <$> areq textField "Texto: " Nothing
+    <*> areq doubleField "Imagem: " Nothing
+--escrever a data    <*> areq intField "Qt Estoque: " Nothing
+
 getHistoriaR :: Handler Html
 getHistoriaR = do 
+    -- GERA O FORMULARIO NA widgetForm
+    (widgetForm, enctype) <- generateFormPost formHistoria
     defaultLayout $ do 
-        addStylesheetRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        $(whamletFile "templates/historia.hamlet")
+        $(whamletFile "templates/Historia.hamlet")
+    
+postHistoriaR :: Handler Html 
+postHistoriaR = do 
+    -- LE O DIGITADO
+    ((res,_),_) <- runFormPost formHistoria
+    case res of 
+        FormSuccess Historia -> do
+            -- INSERE O Historia
+            pid <- runDB $ insert Historia
+            redirect ListaHistoriaR
+        _ -> redirect HomeR
+        
+getListaHistoriaR :: Handler Html
+getListaHistoriaR = do 
+    -- Select * from Historia order by Historia.nome
+    Historias <- runDB $ selectList [] [Asc HistoriaNome]
+    defaultLayout $ do 
+        addStylesheet $ StaticR css_bootstrap_css
+        $(whamletFile "templates/listahistoria.hamlet")
 
+getInfoR :: HistoriaId -> Handler Html
+getInfoR pid = do 
+    Historia <- runDB $ get404 pid
+    defaultLayout $ do 
+        $(whamletFile "templates/historiaid.hamlet")
